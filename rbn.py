73014704a8edd71cb4f4897b98ba4167ef40d673
KN4IIY <61895854+KN4IIY@users.noreply.github.com>
2020-07-24 07:55:31 -0400

    Add files via upload

--- rbn.py
+++ rbn.py
@@ -1,13 +1,18 @@
 #! /usr/bin/python3
 
-from requests_html import HTML
+from requests_html import HTML, HTMLSession
 
-with open('Reverse-Beacon-Network.html') as html_file:
-    source = html_file.read()
-    html = HTML(html=source)
+# with open('Reverse-Beacon-Network.html') as html_file:
+#     source = html_file.read()
+#     html = HTML(html=source)
 
-tbody = html.find('tbody', first=True)
-tr = tbody.find('tr', first=True)
-td = tr.find('td')
+session = HTMLSession()
+r = session.get('http://reversebeacon.net/nodes/')
+r.html.render()
+# tbody = r.html.find('tbody', first=True)
+print()
+# tr = tbody.find('tr', first=True)
+
+# td = tr.find('td')
 # callsign = td
-print(f'Callsign={td[0].text} Gridsquare={td[2].text} Status={td[8].text}')
+# print(f'Callsign = {td[0].text}, Gridsquare = {td[2].text}, Status = {td[8].text}')
